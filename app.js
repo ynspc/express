84799bc56d93d45ca34a7279d8ff999a75802730
Yathartha <yatharta.shrestha@ebpearls.com>
2017-05-25 13:32:09 +0545

    final push

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
+
 app.get('/', function(req, res){
 	res.send("hello there");
 });
 
-app.listen(3000, function(){
-	console.log('listening successfully');
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
